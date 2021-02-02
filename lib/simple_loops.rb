27b6422402ff00a15c089981a8667f537f6889ef
Haley Warson <haleywarson@gmail.com>
2021-02-02 20:43:39 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,38 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  counter = 0
+  
+  while counter <=5 
+    puts string
+    counter += 1
+  end
+end
+
+def loop_message_n_times(string, num)
+  counter = 0
+  
+  while counter <= num 
+    puts string
+    counter += 1
+  end
+end
+
+def output_array(array)
+  counter = 0
+  
+  while counter < array.length
+    puts array
+    counter += 1
+  end
+end
+
+def return_string_array(array)
+  counter = 0
+  
+  new_arr = []
+  while counter < array.length
+    new_arr << counter
+    counter += 1
+  end
+end
